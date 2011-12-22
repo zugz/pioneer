24df3d16f5c1320bf2525bac85d7e74b77764b38
Martin Bays <mbays@sdf.lonestar.org>
2011-12-22 02:24:15 -0500

    crappiness

--- data/shaders/_library.vert.glsl
+++ data/shaders/_library.vert.glsl
@@ -27,3 +27,83 @@ vec2 findSphereEyeRayEntryExitDistance(in vec3 sphereCenter, in vec3 eyeTo, in f
 	return dists;
 }
 */
+
+// opticalDepth: returns estimate of integral of atmosphere density along line
+// vertically upwards to infinity, starting at a point p with height h above
+// the planet in planet-radii, and with y = cos( angle between integration
+// ray and radial ray through p )
+//
+// Estimation is fairly ad-hoc, but is quite cheap and gets good results -
+// according to my tests, ranging over all points and all ADF in the range
+// [50,2000], the worst error is about 10%, and typical error is negligible.
+float _opticalDepth(float h, float y) {
+	// atmosphere density at radius r is exp^(-ADF*(r-1))
+	const float ADF=500.0;
+	// remaining consts are calculated from ADF (in the future, we may want
+	// ADF to vary from planet to planet, in which case these could become
+	// uniforms. Note that in reality, it seems that ADF varies even across
+	// the earth, due to temperature variation...)
+	//
+	// goodsin: sine of angle at which we switch from a linear approximation
+	// to the integral to an exponential one. Experimentally determined
+	// approximate formula for optimal value in terms of ADF:
+	// goodsin = 2*(pow(ADF,-0.5))
+	const float goodsin = 0.0894;
+
+	// goodcos = sqrt(1-goodsin*goodsin)
+	const float goodcos = 0.996;
+
+	// outer: radius at which we consider atmosphere density to be 0
+	const float outer = 1.0+7.0/ADF;
+
+	float r = h-1;
+	float x = sqrt(h*h-y*y);
+    float xsq = x*x;
+
+    float r0 = x/goodcos;
+    float h0 = r0+1;
+    float y0 = h0*goodsin;
+	float outery = sqrt(outer*outer - xsq);
+
+
+	float int1;
+	if (h > outer)
+		return 0.0;
+	else if (abs(y) < y0) {
+		float a0 = (outer/outery-r0/y0)/(outer-r0);
+		float d = y0 - abs(y);
+		return d*exp(-ADF*sqrt(xsq + (y+d/2.0)*(y+d/2.0))) +
+			(exp(-ADF*(h0)) * (r0/y0 - r0*a0 + (r0+(1.0/ADF))*a0))/ADF;
+	}
+    else {
+		float a = (outer/outery-r/y)/(outer-r);
+		return (exp(-ADF*h) * (r/y - r*a + (r+(1.0/ADF))*a))/ADF;
+	}
+}
+float opticalDepth(float h, float y) {
+	if (y<0.0)
+		return 2.0*_opticalDepth(sqrt(h*h-y*y),0) - _opticalDepth(h,-y);
+	else
+		return _opticalDepth(h,y);
+}
+
+float _opticalDepthOReilly(float h, float y) {
+	const float ADF=500.0;
+	float a = 1.0 - y/(h+1.0);
+	// FIXME: wrong constants! They're for ADF=160.
+	return exp(-ADF*h) * exp(-0.00287 + a*(0.459 + a*(3.83 + a*(-6.80 + a*5.25)))) / ADF;
+}
+float _opticalDepthOReilly0(float h) {
+	const float ADF=500.0;
+	// FIXME: wrong constants! They're for ADF=160.
+	//return exp(-ADF*h) * exp(-0.00287 + 0.459 + 3.83 + -6.80 + 5.25) / ADF;
+	return exp(-ADF*h) * 0.030854;
+}
+float opticalDepthOReilly(float h, float y) {
+	float d = _opticalDepthOReilly(h,abs(y));
+	if (y<0.0)
+		// XXX: using h is wrong - should be sqrt( (h+1)*(h+1) - y*y )
+		return 2.0*_opticalDepthOReilly0(h) - d;
+	else
+		return d;
+}
--- data/shaders/geosphere_sky.vert.glsl
+++ data/shaders/geosphere_sky.vert.glsl
@@ -47,8 +47,8 @@ void main(void)
 	float skyNear, skyFar;
 	sphereEntryExitDist(skyNear, skyFar, geosphereCenter, varyingEyepos, geosphereAtmosTopRad);
 	vec3 eyedir = normalize(varyingEyepos);
-	vec3 a = (skyNear * eyedir - geosphereCenter) / geosphereAtmosTopRad;
-	vec3 b = (skyFar * eyedir - geosphereCenter) / geosphereAtmosTopRad;
+	vec3 a = (skyNear * eyedir - geosphereCenter) / geosphereRadius;
+	vec3 b = (skyFar * eyedir - geosphereCenter) / geosphereRadius;
 	vec3 ffac;
 	for (int c=0; c<3; c++)
 		ffac[c] = pow(1.0+float(2-c)/6.0,-4);
@@ -59,44 +59,53 @@ void main(void)
 		vec3 lightDir = normalize(vec3(gl_LightSource[i].position) - geosphereCenter);
 
 		// estimate integral of scattering along the eyeline
-		const int SN = 6;
+		const int SN = 8;
 		vec3 d = (b-a)/float(SN);
 		vec3 scatterInt = vec3(0.0,0.0,0.0);
-		float atmosDensity, lastAtmosDensity, lightAtmosInt;
+		float atmosDensityTimesRad = 0.0;
+		float lastAtmosDensityTimesRad, lightAtmosInt;
+		float secondaryScatterInt, secondaryDensity, secondaryScatter;
 		float scatAtmosInt = 0.0;
 		for (int j=0; j<SN+1; j++) {
 			vec3 p = a+j*d;
-			{
-				// estimate integral of density
-				const int lSN=3;
-				float lb = -dot(p,lightDir);
-				vec3 ld = ((lb + sqrt( lb*lb - dot(p,p) + 1 ))/float(lSN))*lightDir;
-				lightAtmosInt = surfaceDensity*exp(-ADF*(length(p)-0.985));
-				for (int k=1; k<lSN; k++) {
-					vec3 lp = p + k*ld;
-					// 0.985 == 1/ATMOSPHERE_RADIUS
-					lightAtmosInt += 2.0*surfaceDensity*exp(-ADF*(length(lp)-0.985));
-				}
-				lightAtmosInt *= length(ld)*geosphereAtmosTopRad/2.0;
-			}
-
-			lastAtmosDensity = atmosDensity;
-			atmosDensity = surfaceDensity*exp(-ADF*(length(p)-0.985));
+			float r = length(p);
+			if (r < 1.0)
+				continue;
+			float e = exp(-ADF*(r-1.0));
+			lastAtmosDensityTimesRad = atmosDensityTimesRad;
+			atmosDensityTimesRad = geosphereScale*geosphereRadius*surfaceDensity*e;
+			float y = dot(p,lightDir);
+			float a = 1.0 - abs(y)/r;
+			lightAtmosInt = e * exp(-0.00287 + a*(0.459 + a*(3.83 + a*(-6.80 + a*5.25)))) / ADF;
+			if (y < 0.0)
+				lightAtmosInt = 2.0 * exp(-ADF * (sqrt(r*r-y*y)-1.0)) * 0.030854 - lightAtmosInt;
+			lightAtmosInt *= geosphereScale*geosphereRadius*surfaceDensity;
 
 			if (j>0)
-				scatAtmosInt += (lastAtmosDensity + atmosDensity) *
-					length(d)*geosphereAtmosTopRad/2.0;
+				scatAtmosInt += (lastAtmosDensityTimesRad + atmosDensityTimesRad) * r/2.0;
 
-			scatterInt += exp(-(lightAtmosInt+scatAtmosInt)*ffac) *
-				atmosDensity *
+			vec3 sp = p*(1.0+2.0/ADF);
+			float sr = length(sp);
+			float se = exp(-ADF*(sr-1.0));
+			float satmosDensityTimesRad = geosphereScale*geosphereRadius*surfaceDensity*se;
+			float sy = dot(sp,lightDir);
+			float sa = 1.0 - abs(sy)/sr;
+			secondaryScatterInt = se * exp(-0.00287 + sa*(0.459 + sa*(3.83 + sa*(-6.80 + sa*5.25)))) / ADF;
+			// XXX: commented out just to cut down on instructions!
+			//if (sy < 0.0)
+				//secondaryScatterInt = 2.0 * exp(-ADF * (sqrt(sr*sr-sy*sy)-1.0)) * 0.030854 - secondaryScatterInt;
+			secondaryScatterInt *= geosphereRadius*surfaceDensity;
+			secondaryScatter = exp(-(secondaryScatterInt+scatAtmosInt)*ffac) * satmosDensityTimesRad;
+
+			// XXX: this too! I hate shaders!
+			//float lightIntensity = clamp(y / (2.0*lightDiscRadii[0]) + 0.5, 0.0, 1.0);
+
+			scatterInt += ( exp(-(lightAtmosInt+scatAtmosInt)*ffac) * atmosDensityTimesRad +
+					secondaryScatter) *
 				( (j==0||j==SN) ? 1.0 :
-				  (j==1||j==3||j==5) ? 4.0 :
 				  2.0 );
 		}
-		//float lightIntensity = intensityOfLightAtPoint(a+(d*(SN/2))*geosphereRadius/geosphereAtmosTopRad,
-				//lightDir, lightDiscRadii[i], occultedLight == i, occultCentre, srad,
-				//lrad, maxOcclusion);
-		scatterInt *= ffac*gl_LightSource[i].diffuse*length(d)*geosphereAtmosTopRad/3.0;
+		scatterInt *= ffac*gl_LightSource[i].diffuse*length(d)/2.0;
 		for (int c=0; c<3; c++)
 			gl_TexCoord[2][c] += scatterInt[c];
 	}
